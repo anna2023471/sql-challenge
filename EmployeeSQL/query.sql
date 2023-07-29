f7ebf17c2ecf6075aed7fadf80896f23e1577d96
anna2023471 <russiaanna@yahoo.co.nz>
2023-07-29 17:42:16 +1200

    erd, schema, query files

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,54 @@
+-- Q1 list emp. numner, last name, first name, sex, salary of each employee
+SELECT e.emp_no, e.first_name, e.last_name, e.sex
+FROM employee e
+JOIN salary s
+ON (e.emp_no = s.emp_no);
+
+-- Q2 list first name, last name, hire date for emps. hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employee
+WHERE SPLIT_PART(hire_date, '/', 3) = '1986';
+
+-- Q3 list manager of each dpt. with dept. no, dept, name, emp. no, last name, first name
+SELECT d.dept_name, d.dept_no, e.emp_no, e.last_name, e.first_name
+FROM employee e
+JOIN dept_manager AS dm ON (e.emp_no = dm.emp_no)
+JOIN department AS d ON (d.dept_no = dm.dept_no);
+
+-- Q4 list dept. no of each emp., with emp. no, last name, first name, dept name
+SELECT d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employee e
+JOIN dept_employee AS de ON (e.emp_no = de.emp_no)
+JOIN department AS d ON (d.dept_no = de.dept_no);
+
+-- Q5 list first name, last name, sex of each emp. whose first name is Hercules 
+-- and last name begins with B.
+SELECT first_name, last_name, sex
+FROM employee
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- Q6 list each emp. in Sales, inlc. emp. no, last name, first name
+SELECT emp_no, last_name, first_name
+FROM employee
+WHERE emp_no IN
+	(SELECT emp_no
+	FROM dept_employee
+	WHERE dept_no IN 
+		(SELECT dept_no
+		FROM department
+		WHERE dept_name = 'Sales'));
+		
+-- Q7 list each emp in Sales and Development, incl. emp no, last name, first name,
+-- dept. name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employee e
+JOIN dept_employee AS de ON (de.emp_no = e.emp_no)
+JOIN department AS d ON (d.dept_no = de.dept_no)
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+-- Q8 list frequency counts, in desc. order, of all emp. last names
+SELECT last_name, COUNT(emp_no) AS "Total Employees"
+FROM employee
+GROUP BY last_name
+ORDER BY "Total Employees" DESC;
+	
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,78 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/0aNS45
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "department" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "title" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employee" (
+    "emp_no" VARCHAR(6)   NOT NULL,
+    "emp_title" VARCHAR(5)   NOT NULL,
+    "birth_date" VARCHAR   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salary" (
+    "emp_no" VARCHAR(6)   NOT NULL,
+    "salary" INTEGER   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "emp_no" VARCHAR(6)   NOT NULL
+);
+
+CREATE TABLE "dept_employee" (
+    "emp_no" VARCHAR(6)   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL
+);
+
+ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "title" ("title_id");
+
+ALTER TABLE "salary" ADD CONSTRAINT "fk_salary_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+SELECT * from department;
+
+SELECT * from title;
+
+SELECT * from salary;
+
+SELECT * from employee;
+
+SELECT * from dept_employee;
+
+SELECT * from dept_manager;
